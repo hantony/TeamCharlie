834e0cd9a72225c656ad5b2e6e9a5e1aca25e7fc
hmant <hmant@host.docker.internal>
2025-06-16 23:19:02 -0400

    Test updates

--- src/test/java/com/umgc/AttendanceLogTest.java
+++ src/test/java/com/umgc/AttendanceLogTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MySQLContainer;
